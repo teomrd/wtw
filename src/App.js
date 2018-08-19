69c8ff46211e0c57737af557c250b24b67c75487
teomrd <mironidis.gr@gmail.com>
2018-08-19 21:02:07 +0100

    Theo #000 Branding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import "./App.css";
 import Forecast from "./components/pages/Forecast/Forecast";
 
@@ -6,6 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App blue-skies-gradient">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Whatever The Weather</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <Forecast />
       </div>
     );
